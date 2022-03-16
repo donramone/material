17d34204aaafd0e91ea6f7d1ad9fcfaedd6c291e
Luigi <luigi_lipreri@hotmail.com>
2022-03-16 00:22:38 -0300

    Agregado Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: '**', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'dashboard', loadChildren: () => import('./components/dashboard/dashboard.module').then( (x) => x.DashboardModule) },
+  { path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
